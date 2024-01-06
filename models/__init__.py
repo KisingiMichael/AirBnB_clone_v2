010ab32b7c17aad7cd852f8489602d94237cad11
KisingiMichael <kisingimichael12@gmail.com>
2024-01-05 17:01:46 -0800

    Fix cycular import error

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an instance of the Storage will be used"""
 
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from os import getenv
+import os
 
-StorageType = getenv('HBNB_TYPE_STORAGE')
-if StorageType == 'db':
+storage = os.getenv('HBNB_TYPE_STORAGE')
+if storage == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
+
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
