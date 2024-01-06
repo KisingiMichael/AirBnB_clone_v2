aeeb7fa1d68a342606c0f1321b1a9b4a5e743cac
KisingiMichael <kisingimichael12@gmail.com>
2024-01-05 16:37:24 -0800

    fix datetime errors

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel:
         updated_at (sqlalchemy DateTime): last update datetime
     """
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initializes a new BaseModel instance"""
