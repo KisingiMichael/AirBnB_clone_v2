e37c9c644456cd90bc419fb0ece4471e83700ebe
KisingiMichael <kisingimichael12@gmail.com>
2024-01-05 16:53:35 -0800

    fix amenity blocken pipes

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,9 @@ from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
@@ -14,5 +17,6 @@ class Amenity(BaseModel, Base):
     """
 
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
