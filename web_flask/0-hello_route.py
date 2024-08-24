638043db420249877db58e63e523bf681b6aef5c
KisingiMichael <kisingimichael12@gmail.com>
2024-08-24 20:46:44 +0000

    Add files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,14 +2,14 @@
 """Start a flask web application"""
 
 from flask import Flask
+
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """Return Hello HBNB"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """stsrt the flask developement server"""
     app.run(host='0.0.0.0', port=5000)
